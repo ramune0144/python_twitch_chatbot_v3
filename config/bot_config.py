68af6f41f7c004ee76eb505defd4f51846f65c0d
Rabbit101 <70846170+ramune0144@users.noreply.github.com>
2022-09-10 22:40:38 +0700

    Update bot_config.py

--- config/bot_config.py
+++ config/bot_config.py
@@ -1,7 +1,8 @@
 class BotConfig:
-    TMI_TOKEN = "oauth:"
-    SECRET = ''
-    CLIENT_ID=""
+    TMI_TOKEN = "oauth:" # Replace with your bot's token
+    SECRET = '' # Replace with your bot's secret
+    CLIENT_ID="" # Replace with your bot's client ID
     BOT_PREFIX = "!"
-    CHANNEL = [""]
-    ADMIN=["",""]
+    CHANNEL = [""] # Replace with your channel only one channel
+    ADMIN=[""] #name of the admin can add  more then one ["admin1","admin2"]
+    Ignore_User = ["streamelements","nightbot","moobot"] #name of the user that you want to ignore
